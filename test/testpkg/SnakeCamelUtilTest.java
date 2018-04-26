1850bedb77e090817c915a73d000e31cd4b5bfbb
yamagoehiroko <yamagoehiroko@172.19.10.1>
2018-04-26 14:26:37 +0900

    テストパッケージ作ったよ

---
+++ test/testpkg/SnakeCamelUtilTest.java
@@ -0,0 +1,14 @@
+package testpkg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelUtilTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
