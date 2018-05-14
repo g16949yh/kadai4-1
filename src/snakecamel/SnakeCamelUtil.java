d1cc29403b4885fed222761a1162eaaa4fd2f617
yamagoehiroko <yamagoehiroko@172.19.15.106>
2018-05-14 12:59:36 +0900

    Main.javaを追加しました

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -3,10 +3,10 @@ package snakecamel;
 public class SnakeCamelUtil {
 
 	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+		String[] words = snake_case.split("_");//_で区切る
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
-			sb.append(capitalize(words[i]));
+		for (int i = 0; i < words.length; i++) {
+			sb.append(capitalize(words[i]));//大文字にする
 		}
 		return new String(sb);
 	}
@@ -15,28 +15,28 @@ public class SnakeCamelUtil {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
+			char c = camelcase.charAt(i);//一文字区切りにする
 			if (Character.isUpperCase(c)) {
 				sb.append(camelcase.substring(j, i));
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	public static String capitalize(String s) {
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
 		return upperFirst + rest;
 	}
 
-	static String uncapitalize(String s) {
+	public static String uncapitalize(String s) {
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
--- test/testpkg/SnakeCamelUtilTest.java
+++ test/testpkg/SnakeCamelUtilTest.java
@@ -1,14 +1,44 @@
 package testpkg;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
+
+import snakecamel.SnakeCamelUtil;
 
 public class SnakeCamelUtilTest {
 
 	@Test
-	public void test() {
-		fail("まだ実装されていません");
-	}
+	public void snakeToCamelcaseでスネークケースの文字列をキャメルケースに変換します(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "AbcDefGhi";
+		String actual = scu.snakeToCamelcase("abc_def_ghi");
+		assertThat(actual,is(expected));
+		}
+	@Test
+	public void camelToSnakecaseでキャメルケースの文字列をスネークケースに変換します(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "abc_def_ghi";
+		String actual = scu.camelToSnakecase("AbcDefGhi");
+		assertThat(actual,is(expected));
+		}
+	@Test
+	public void capitalizeで大文字に変換します(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "Abc";
+		String actual = scu.capitalize("abc");
+		assertThat(actual,is(expected));
+		}
+	@Test
+	public void uncapitalizeで小文字に変換します(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "abc";
+		String actual = scu.uncapitalize("Abc");
+		assertThat(actual,is(expected));
+		}
+
+
+
+
 
 }
